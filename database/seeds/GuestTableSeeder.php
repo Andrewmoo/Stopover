0e09ad24a2460dd4b4d0b80b6f2aa0ceeff93fd2
Andrewmoo <andrewmoo98@gmail.com>
2019-02-05 17:24:05 +0000

    Update GuestTableSeeder.php

--- database/seeds/GuestTableSeeder.php
+++ database/seeds/GuestTableSeeder.php
@@ -18,6 +18,7 @@ class GuestTableSeeder extends Seeder
         $guest->email = 'guest@guest.com';
         $guest->address = '15 Sesh Lane';
         $guest->institution = 'IADT';
+        $guest->phone = "0838756646";
         $guest->save();
     }
 }
