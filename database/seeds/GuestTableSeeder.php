6a34a9319959a9fb40fea0a56d7d1062160779f3
frangild <32389531+frangild@users.noreply.github.com>
2019-02-22 15:26:45 +0000

    Update GuestTableSeeder.php

--- database/seeds/GuestTableSeeder.php
+++ database/seeds/GuestTableSeeder.php
@@ -18,7 +18,6 @@ class GuestTableSeeder extends Seeder
         $guest->email = "guest@one.com";
         $guest->address = "15 Limekiln Lane";
         $guest->phone = "+353 85 6767354";
-        $guest->institution = "+353 85 6767354";
         $guest->user_id = "1";
         $guest->save();
     }
